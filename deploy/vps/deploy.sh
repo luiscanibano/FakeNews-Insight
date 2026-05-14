9158379081790a6dce5fd98a035dfc980e1246e5
Luis Cañibano Mateos <luiscanibanomateos@gmail.com>
2026-05-14 11:24:46 +0200

    Make VPS deploy resilient to local hotfixes

--- deploy/vps/deploy.sh
+++ deploy/vps/deploy.sh
@@ -7,6 +7,11 @@ BRANCH="${BRANCH:-main}"
 cd "$APP_DIR"
 git fetch origin
 git checkout "$BRANCH"
+
+if ! git diff --quiet -- deploy/vps/Caddyfile deploy/vps/deploy.sh; then
+	git stash push -m "auto-deploy-stash" -- deploy/vps/Caddyfile deploy/vps/deploy.sh
+fi
+
 git pull --ff-only origin "$BRANCH"
 git lfs pull
 sudo docker compose -f deploy/vps/docker-compose.yml --env-file deploy/vps/.env up -d --build
